ffeb1097de9541efa309dbcd725687265e8d4bda
Adelina <adelinama200@gmail.com>
2022-11-03 21:56:57 +0300

    Add tasks generation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import logging
+from pprint import pprint
+import random
 
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from sdam_gia import SdamGIA
+
+from telegram import Update, ForceReply, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
 
 # Enable logging
 logging.basicConfig(
@@ -9,20 +13,96 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+SUBJECT = 'inf'
 
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Вариант", callback_data="variant"),
+            InlineKeyboardButton("Задачи", callback_data="zadacha"),
+        ]
+    ]
+    keyboard_markup = InlineKeyboardMarkup(keyboard)
+
     user = update.effective_user
     update.message.reply_text(
         f"Привет, {user.full_name}!\n"
         f"Это бот для подготовки к ОГЭ по информатике.\n"
         f"Чтобы начать решение варианта нажми на кнопку Вариант.\n"
-        f"Чтобы начать решать случайные задачи нажми на Задача.\n",
-        reply_markup=ForceReply(selective=True),
+        f"Чтобы начать решать случайные задачи нажми на Задачи.\n",
+        reply_markup=keyboard_markup,
     )
+
+
+def button(update: Update, context: CallbackContext):
+    # обработка кнопок
+    query = update.callback_query
+    callbackdata = query.data
+
+    if callbackdata == "variant":
+        print("вы тыкнули на кнопку вариант")
+    elif callbackdata == "zadacha":
+        keyboard = [
+            [
+                InlineKeyboardButton("1", callback_data="z1"),
+                InlineKeyboardButton("2", callback_data="z2"),
+                InlineKeyboardButton("3", callback_data="z3")
+            ],
+            [
+                InlineKeyboardButton("4", callback_data="z4"),
+                InlineKeyboardButton("5", callback_data="z5"),
+                InlineKeyboardButton("6", callback_data="z6"),
+            ],
+            [
+
+                InlineKeyboardButton("7", callback_data="z7"),
+                InlineKeyboardButton("8", callback_data="z8"),
+                InlineKeyboardButton("9", callback_data="z9"),
+            ],
+            [
+                InlineKeyboardButton("10", callback_data="z10"),
+                InlineKeyboardButton("11", callback_data="z11"),
+                InlineKeyboardButton("12", callback_data="z12"),
+            ]
+        ]
+        keyboard_markup = InlineKeyboardMarkup(keyboard)
+        sdamgia = SdamGIA()
+        catalog = sdamgia.get_catalog(SUBJECT)
+        message = "Выберите номер задачи.\n"
+        for i, topic in enumerate(catalog):
+            message += f"{i + 1} - {topic['topic_name']}\n"
+        query.edit_message_text(
+            text=message,
+            reply_markup=keyboard_markup
+        )
+    elif callbackdata.startswith('z'):
+        # Из кода кнопки получаем номер
+        topic_index = int(callbackdata[1:])
+        # Получаем список тем
+        sdamgia = SdamGIA()
+        catalog = sdamgia.get_catalog(SUBJECT)
+        # Получение всех задач темы
+        tasks = sdamgia.get_category_by_id(SUBJECT, catalog[topic_index]['topic_id'])
+        # Выбор случайной задачи
+        task = sdamgia.get_problem_by_id(SUBJECT, tasks[random.randint(0, len(tasks))])
+
+        pprint(task)
+
+        message = f"Задача {task['id']}\n" + f"{task['condition']['text']}\n"
+        keyboard = [
+            [
+                InlineKeyboardButton("Cледующая задача", callback_data=f"z{topic_index}"),
+            ]
+        ]
+        keyboard_markup = InlineKeyboardMarkup(keyboard)
+        query.edit_message_text(
+            text=message,
+            reply_markup=keyboard_markup
+        )
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -46,7 +126,7 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
+    dispatcher.add_handler(CallbackQueryHandler(button))
     # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
